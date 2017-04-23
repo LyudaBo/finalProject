9a4d113b58ef86def3664b9d19249edaf4b07fa5
firsachi <firsachi.alex@gmail.com>
2017-04-23 21:51:38 +0300

    Modified entity. Add generic.

--- src/main/java/com/gojava/dao/RoomDao.java
+++ src/main/java/com/gojava/dao/RoomDao.java
@@ -1,6 +1,13 @@
 package com.gojava.dao;
 
-public interface RoomDao {
+import java.util.Set;
+
+public interface RoomDao<T>{
 
     //Редактировать данные комнаты
+	void add(T entity);
+	void update(T entity);
+	void delete(T entity);
+	T find(long id);
+	Set<T> all();
 }
