5734bffd4a919800c88fca186ed000760e170006
ArtyrRoze <a.r.c.h.y@rambler.ru>
2017-04-23 14:48:07 +0300

    1st impl branch

--- src/main/java/com/gojava/dao/impl/HotelDaoImpl.java
+++ src/main/java/com/gojava/dao/impl/HotelDaoImpl.java
@@ -1,6 +1,21 @@
 package com.gojava.dao.impl;
 
 import com.gojava.dao.HotelDao;
+import com.gojava.model.Hotel;
+import com.gojava.model.Room;
+import com.gojava.model.User;
 
 public class HotelDaoImpl implements HotelDao {
+
+    public Room findRoomByHotel(Hotel hotel) {
+        return null;
+    }
+
+    public boolean bookRoom(Room room, User user) {
+        return false;
+    }
+
+    public boolean removeBookRoom(Room room) {
+        return false;
+    }
 }
