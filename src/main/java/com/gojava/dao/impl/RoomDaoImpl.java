3c37311510f489c0c6e145305a7f2107ac20c7b4
firsachi <firsachi.alex@gmail.com>
2017-04-23 22:02:32 +0300

    +

--- src/main/java/com/gojava/dao/impl/RoomDaoImpl.java
+++ src/main/java/com/gojava/dao/impl/RoomDaoImpl.java
@@ -1,6 +1,41 @@
 package com.gojava.dao.impl;
 
-import com.gojava.dao.RoomDao;
+import java.util.Set;
 
-public class RoomDaoImpl implements RoomDao {
+import com.gojava.dao.RoomDao;
+import com.gojava.model.Room;
+
+public class RoomDaoImpl implements RoomDao<Room> {
+
+	@Override
+	public void add(Room entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Room entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Room entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Room find(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Room> all() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 }
