171cf5c60c06f827f7db85ca31ec58ba92d5403a
firsachi <firsachi.alex@gmail.com>
2017-04-23 17:41:39 +0300

    add parameters entity

--- src/main/java/com/gojava/model/Hotel.java
+++ src/main/java/com/gojava/model/Hotel.java
@@ -1,8 +1,58 @@
 package com.gojava.model;
 
 import java.io.Serializable;
+import java.util.Set;
+
+/**
+ * Simple JavaBean domain object that represents a Room in a hotel.
+ *
+ * @author Firsov
+ * @version 1.0
+ */
 
 public class Hotel implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+
+	private long id;
+	private String name;
+	private Set<Room> rooms;
+	private String city;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Set<Room> getRooms() {
+		return rooms;
+	}
+	
+	public void setRooms(Set<Room> rooms) {
+		this.rooms = rooms;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
 }
