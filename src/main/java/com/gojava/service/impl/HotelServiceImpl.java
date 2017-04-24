72232c3f26e1a698412da79f829bee04bde87c94
ArtyrRoze <a.r.c.h.y@rambler.ru>
2017-04-24 21:35:23 +0300

    added CRUD operation to Hotel

--- src/main/java/com/gojava/service/impl/HotelServiceImpl.java
+++ src/main/java/com/gojava/service/impl/HotelServiceImpl.java
@@ -1,6 +1,7 @@
 package com.gojava.service.impl;
 
 import com.gojava.dao.HotelDao;
+import com.gojava.dao.impl.HotelDaoImpl;
 import com.gojava.model.Hotel;
 import com.gojava.model.Room;
 import com.gojava.model.User;
@@ -8,7 +9,22 @@ import com.gojava.service.HotelService;
 
 public class HotelServiceImpl implements HotelService {
 
-    private HotelDao hotelDao;
+    private HotelDao hotelDao = new HotelDaoImpl();
+
+    @Override
+    public Hotel addHotel(Hotel hotel) {
+        return hotelDao.addHotel(hotel);
+    }
+
+    @Override
+    public Hotel editHotel(Hotel hotel) {
+        return hotelDao.editHotel(hotel);
+    }
+
+    @Override
+    public boolean deleteHotel(Hotel hotel) {
+        return hotelDao.deleteHotel(hotel);
+    }
 
     @Override
     public Room findRoomByHotel(Hotel hotel) {
