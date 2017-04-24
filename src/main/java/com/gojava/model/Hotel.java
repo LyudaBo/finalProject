2da1f433558935f5bbff41a588f68b5d0ebfd45b
firsov <firsov@firs.kievreclama.local>
2017-04-24 09:10:59 +0300

    add method equals and hashCode and to String

--- src/main/java/com/gojava/model/Hotel.java
+++ src/main/java/com/gojava/model/Hotel.java
@@ -54,5 +54,51 @@ public class Hotel implements Serializable {
 	public void setCity(String city) {
 		this.city = city;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((rooms == null) ? 0 : rooms.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Hotel other = (Hotel) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (rooms == null) {
+			if (other.rooms != null)
+				return false;
+		} else if (!rooms.equals(other.rooms))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Hotel [id=" + id + ", name=" + name + ", rooms=" + rooms + ", city=" + city + "]";
+	}
+	
 	
 }
--- src/main/java/com/gojava/model/Room.java
+++ src/main/java/com/gojava/model/Room.java
@@ -21,7 +21,8 @@ public class Room implements Serializable {
 	private int number;
 	private boolean framed;
 	private boolean delete;
-	
+	private long hotel;
+
 	public long getId() {
 		return id;
 	}
@@ -53,4 +54,53 @@ public class Room implements Serializable {
 	public void setDelete(boolean delete) {
 		this.delete = delete;
 	}
+	
+	public long getHotel() {
+		return hotel;
+	}
+
+	public void setHotel(long hotel) {
+		this.hotel = hotel;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (delete ? 1231 : 1237);
+		result = prime * result + (framed ? 1231 : 1237);
+		result = prime * result + (int) (hotel ^ (hotel >>> 32));
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + number;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Room other = (Room) obj;
+		if (delete != other.delete)
+			return false;
+		if (framed != other.framed)
+			return false;
+		if (hotel != other.hotel)
+			return false;
+		if (id != other.id)
+			return false;
+		if (number != other.number)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Room [id=" + id + ", number=" + number + ", framed=" + framed + ", delete=" + delete + ", hotel="
+				+ hotel + "]";
+	}
+	
 }
