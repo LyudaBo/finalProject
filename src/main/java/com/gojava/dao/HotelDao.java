eccc18fea14831a302489c8a55c3332780156327
ArtyrRoze <a.r.c.h.y@rambler.ru>
2017-04-24 14:18:18 +0300

    added 3 methods to HotelDao

--- src/main/java/com/gojava/dao/HotelDao.java
+++ src/main/java/com/gojava/dao/HotelDao.java
@@ -4,18 +4,19 @@ import com.gojava.model.Hotel;
 import com.gojava.model.Room;
 import com.gojava.model.User;
 
-public interface HotelDao {
+public interface HotelDao<T> {
 
     //Добавить отель
+    void addHotel(T hotel);
+
     //Редактировать данные отеля
-    //Добавить комнату в отель
-
-
-    //Удалить комнату из отеля
+    void update(T hotel);
 
     //Удалить отель
+    boolean deleteHotel(T hotel);
 
-
+    //Добавить комнату в отель
+    //Удалить комнату из отеля
     //Поиск отеля по имени
     //Поиск отеля по городу
     //Поиск комнаты по отелю
