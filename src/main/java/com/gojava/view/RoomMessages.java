6a60c9a384f168d85ddc2bcb594b7e1bd3313341
firsachi <firsachi@Admin>
2017-04-29 14:10:56 +0300

    create view class and message room

---
+++ src/main/java/com/gojava/view/RoomMessages.java
@@ -0,0 +1,15 @@
+package com.gojava.view;
+
+
+/**
+ * Simple JavaBean domain object that represents a Room in a hotel.
+ *
+ * @author Firsov
+ * @version 1.0
+ */
+public class RoomMessages {
+
+	public final static String ADD_ROOM = "Input new room.";
+	public final static String INPUT_NUMBER = "Input number room";
+	
+}
---
+++ src/main/java/com/gojava/view/View.java
@@ -0,0 +1,9 @@
+package com.gojava.view;
+
+public class View {
+	
+	public void message(String message){
+		System.out.println(message);
+	}
+
+}
